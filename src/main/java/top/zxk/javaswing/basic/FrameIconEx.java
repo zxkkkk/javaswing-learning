2675761f982d63d31c40e4841a78e12858ad9948
zxkkkk <858654746@qq.com>
2023-03-01 23:20:40 +0800

    example,Quit,Icon

--- src/main/java/top/zxk/javaswing/basic/FrameIconEx.java
+++ src/main/java/top/zxk/javaswing/basic/FrameIconEx.java
@@ -1,2 +1,33 @@
-package top.zxk.javaswing.basic;public class FrameIconEx {
+package top.zxk.javaswing.basic;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameIconEx extends JFrame {
+
+    public FrameIconEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        var webIcon = new ImageIcon("src/resources/web.png");
+
+        setIconImage(webIcon.getImage());
+
+        setTitle("Icon");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new FrameIconEx();
+            ex.setVisible(true);
+        });
+    }
 }
--- src/main/java/top/zxk/javaswing/basic/QuitButtonEx.java
+++ src/main/java/top/zxk/javaswing/basic/QuitButtonEx.java
@@ -1,2 +1,51 @@
-package top.zxk.javaswing.basic;public class QuitButtonEx {
+package top.zxk.javaswing.basic;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class QuitButtonEx extends JFrame {
+
+    public QuitButtonEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+        var quitButton = new JButton("Quit");
+
+        quitButton.addActionListener((event) -> System.exit(0));
+
+        createLayout(quitButton);
+
+        setTitle("Quit button");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        var pane = getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new QuitButtonEx();
+            ex.setVisible(true);
+        });
+    }
 }
